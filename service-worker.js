78d5c6faa9a88faae6269ff27e4f0faa5c6df5fc
IPCET <131810497+SISTEMAPCTAB@users.noreply.github.com>
2026-09-01 13:20:32 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='refugios-ipcet-v125';
+const CACHE='refugios-ipcet-v126';
 const CORE=['./','./index.html','./manifest.webmanifest','./validar.html','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
